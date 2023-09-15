5e470a7b263f391387b9b5c27740167933d8cf15
M-Parsaei <khodadostetdarm@gmail.com>
2023-09-15 00:25:01 -0700

    finished the styling for the register page

---
+++ client/src/components/Background.js
@@ -0,0 +1,4 @@
+import styles from "./Background.module.css"
+export default function Background(){
+    return(<div id={styles['background-component']}></div>);
+}
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,24 +1,49 @@
 import { useState } from "react";
+import styles from "./Register.module.css";
 
-function Register(){
-    const [username,setUsername]= useState('');
-    const [password,setPassword]= useState('');
+function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-
-    function handleSubmit(e){
-        e.preventDefault();
-        console.log('button was pressed')
-    }
-    return(
-        <form onSubmit={handleSubmit}>
-            <h2>Name</h2>
-            <input type='text' name="username" placeholder="Username" value={''} onChange={e=>{setUsername(e.target.value)}} ></input>
-            <input type='password' name="password" placeholder="Password" value={''} onChange={e=>{setPassword(e.target.value)}} ></input>
-            <input type='password' name="confirmPassword" placeholder="Confirm Password" value={''} ></input>
-            <button type="submit">Submit</button>
-            <p>have an account already? <a href="/login">Login</a></p>
-        </form>
-    );
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("button was pressed");
+  }
+  return (
+    <div id={styles["register-container"]}>
+      <form className={styles["register-form"]} onSubmit={handleSubmit}>
+        <span>Name</span>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={""}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        ></input>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={""}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          value={""}
+        ></input>
+        <button type="submit">Submit</button>
+        <span>have an account already? &nbsp;
+          <a href="/login"> Login</a>
+        </span>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
